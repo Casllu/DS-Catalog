6fadff9d8eb65950fca22def4bbd3070d2893b48
Lucas Almeida <lalmeida464@gmail.com>
2025-01-13 22:10:34 -0300

    CategoryResource

---
+++ backend/src/main/java/com/lucasalmeida/dscatalog/entities/Category.java
@@ -0,0 +1,51 @@
+package com.lucasalmeida.dscatalog.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity(name = "tb_category")
+public class Category implements Serializable {
+    @Id
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
