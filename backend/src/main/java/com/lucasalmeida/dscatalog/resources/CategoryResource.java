195ee71d93685240a8b28e50b0a74b6bc92a5c7b
Lucas Almeida <lalmeida464@gmail.com>
2025-01-20 13:46:46 -0300

    DTO

--- backend/src/main/java/com/lucasalmeida/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/lucasalmeida/dscatalog/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.lucasalmeida.dscatalog.resources;
 
+import com.lucasalmeida.dscatalog.dto.CategoryDTO;
 import com.lucasalmeida.dscatalog.entities.Category;
 import com.lucasalmeida.dscatalog.servicies.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping()
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
 
         return ResponseEntity.ok().body(list);
 
