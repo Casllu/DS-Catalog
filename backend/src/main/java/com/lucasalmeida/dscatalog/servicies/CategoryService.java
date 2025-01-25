dcd8a17d3de58d6d8d07dc20bc1820508fe61cc3
Lucas Almeida <lalmeida464@gmail.com>
2025-01-25 15:00:10 -0300

    Delete category

--- backend/src/main/java/com/lucasalmeida/dscatalog/servicies/CategoryService.java
+++ backend/src/main/java/com/lucasalmeida/dscatalog/servicies/CategoryService.java
@@ -3,10 +3,14 @@ package com.lucasalmeida.dscatalog.servicies;
 import com.lucasalmeida.dscatalog.dto.CategoryDTO;
 import com.lucasalmeida.dscatalog.entities.Category;
 import com.lucasalmeida.dscatalog.repository.CategoryRepository;
+import com.lucasalmeida.dscatalog.servicies.exceptions.DatabaseException;
 import com.lucasalmeida.dscatalog.servicies.exceptions.ResourceNotFoundException;
+
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -52,6 +56,19 @@ public class CategoryService {
             return new CategoryDTO(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
         }
     }
 }
---
+++ backend/src/main/java/com/lucasalmeida/dscatalog/servicies/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.lucasalmeida.dscatalog.servicies.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
