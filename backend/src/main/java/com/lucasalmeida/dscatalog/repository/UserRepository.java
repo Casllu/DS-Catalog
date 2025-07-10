36353c4015f7036699c94feb77cf98f74752f866
Lucas <lalmeida464@gmail.com>
2025-07-10 13:54:30 -0300

    User CRUD

---
+++ backend/src/main/java/com/lucasalmeida/dscatalog/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.lucasalmeida.dscatalog.repository;
+
+import com.lucasalmeida.dscatalog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ backend/src/main/java/com/lucasalmeida/dscatalog/servicies/UserService.java
@@ -0,0 +1,101 @@
+package com.lucasalmeida.dscatalog.servicies;
+
+import com.lucasalmeida.dscatalog.dto.CategoryDTO;
+import com.lucasalmeida.dscatalog.dto.RoleDTO;
+import com.lucasalmeida.dscatalog.dto.UserDTO;
+import com.lucasalmeida.dscatalog.dto.UserInsertDTO;
+import com.lucasalmeida.dscatalog.entities.Category;
+import com.lucasalmeida.dscatalog.entities.Role;
+import com.lucasalmeida.dscatalog.entities.User;
+import com.lucasalmeida.dscatalog.repository.CategoryRepository;
+import com.lucasalmeida.dscatalog.repository.RoleRepository;
+import com.lucasalmeida.dscatalog.repository.UserRepository;
+import com.lucasalmeida.dscatalog.servicies.exceptions.DatabaseException;
+import com.lucasalmeida.dscatalog.servicies.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<UserDTO> findAllPaged(Pageable pageRequest) {
+        Page<User> page = userRepository.findAll(pageRequest);
+        return page.map(UserDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+
+        return new UserDTO(entity);
+    }
+
+    @Transactional
+    public UserDTO insert(UserInsertDTO dto) {
+        User entity = new User();
+        copyDtoToEntity(dto, entity);
+        entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+        entity = userRepository.save(entity);
+
+        return new UserDTO(entity);
+    }
+
+    @Transactional
+    public UserDTO update(Long id, UserDTO dto) {
+        try {
+            User entity = userRepository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = userRepository.save(entity);
+
+            return new UserDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+
+        if (!userRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            userRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void copyDtoToEntity(UserDTO dto, User entity) {
+        entity.setFirstName(dto.getFirstName());
+        entity.setLastName(dto.getLastName());
+        entity.setEmail(dto.getEmail());
+
+        entity.getRoles().clear();
+        for (RoleDTO roleDTO : dto.getRoles()) {
+            Role role = roleRepository.getReferenceById(roleDTO.getId());
+            entity.getRoles().add(role);
+        }
+    }
+}
